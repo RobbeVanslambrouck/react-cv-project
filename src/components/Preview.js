40dce916e665972fcbb5b3afed9b9c062b27a5e6
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-08-01 02:33:01 +0200

    adding new components

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import Education from "./cvElements/Education";
 import Experience from "./cvElements/Experience";
 import GeneralInfo from "./cvElements/GeneralInfo";
+import CvTop from "./cvElements/CvTop";
+import Contact from "./cvElements/Contact";
+import Profile from "./cvElements/Profile";
 import "../styles/preview.scss";
 
 class Preview extends Component {
@@ -9,6 +12,9 @@ class Preview extends Component {
     const { educations, experiences, personalInfo } = this.props;
     return (
       <div className="preview">
+        <CvTop info={personalInfo}></CvTop>
+        <Contact info={personalInfo}></Contact>
+        <Profile info={personalInfo}></Profile>
         <GeneralInfo info={personalInfo} />
         <Experience experiences={experiences} />
         <Education educations={educations} />
