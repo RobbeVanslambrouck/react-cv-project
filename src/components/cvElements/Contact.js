5672e2a43ebdda85a2658621ce1de1f991cd3a5a
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-08-11 23:01:06 +0200

    completed missing contact items and icons

--- src/components/cvElements/Contact.js
+++ src/components/cvElements/Contact.js
@@ -2,14 +2,18 @@ import React, { Component } from "react";
 
 class Contact extends Component {
   render() {
-    const { email, phoneNumber } = this.props.info;
+    const { email, phoneNumber, location, websiteLink, linkedinLink } =
+      this.props.info;
     return (
       <div className="contact">
         <p className="section-title">Contact</p>
-        <p className="email">{email}</p>
-        {isNaN(phoneNumber) ? null : (
+        {phoneNumber ? (
           <p className="phone-number">{phoneNumber.toString()}</p>
-        )}
+        ) : null}
+        {email ? <p className="email">{email}</p> : null}
+        {location ? <p className="location">{location}</p> : null}
+        {linkedinLink ? <p className="linkedin">{linkedinLink}</p> : null}
+        {websiteLink ? <p className="website">{websiteLink}</p> : null}
       </div>
     );
   }
